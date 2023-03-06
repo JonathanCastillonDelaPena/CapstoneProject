fa94f99ab5f47dba442454caecca0b0245939dd0
Jonathan C Dela Peña <jonathancastillondelapena28@gmail.com>
2023-03-06 20:41:36 +0800

    Added connection to the deployed server in Cyclic

--- src/dataServices/dbServerURI.js
+++ src/dataServices/dbServerURI.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 // Use this for local testing
-export default axios.create({ baseURL: "http://localhost:3000" });
+// export default axios.create({ baseURL: "http://localhost:3000" });
 
 // Deployed database server
-// export default axios.create({
-//   baseURL: "",
-// });
+export default axios.create({
+  baseURL: "https://capstone-server.cyclic.app",
+});
