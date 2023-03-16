fb20d473e9da1378e9c0c89e97934b2f004a20a4
Josii26 <josephinfiesto0@gmail.com>
2023-03-16 09:54:14 +0800

    settingUpProfile

--- src/pageRouter/RoutesController.jsx
+++ src/pageRouter/RoutesController.jsx
@@ -5,15 +5,16 @@ import { Route, Routes } from "react-router-dom";
 import NotFoundWarningPage from "../pages/NotFoundWarningPage";
 import HomePage from "../pages/HomePage";
 import PageLogin from "../pages/PageLogin";
-
+import SettingUpAcc from "../pages/SettingUpAcc";
 
 const RoutesController = () => {
   return (
     <Routes>
-      <Route index element={<HomePage />} />
+      <Route index element={<SettingUpAcc/>} />
       <Route path="*" element={<NotFoundWarningPage />} />
       <Route path="/login" element={<PageLogin />} />
       <Route path="/home" element={<HomePage />} />
+      <Route path="/setting" element={<SettingUpAcc />} />
     </Routes>
   );
 };
