17c84a5a73ceeb9b608c879383d8d44c97b71099
Jonathan C Dela Peña <jonathancastillondelapena28@gmail.com>
2023-03-23 21:49:44 +0800

    Implementing: Clickable user search results

--- src/components/searchBox/SearchResultCard.jsx
+++ src/components/searchBox/SearchResultCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SearchResultCard = ({ props }) => {
   return (
-    <div className="card mt-2">
-      <div className="card-body">
-        <img className="miniProfile" src={props.image_url} />
-        <p>First Name: {props.first_name}</p>
-        <p>Last Name: {props.last_name}</p>
-        <p>Gender: {props.gender}</p>
-      </div>
-    </div>
+    <Link to={"/home"}>
+        <div className="card mt-2">
+          <div className="card-body">
+            <img className="miniProfile" src={props.image_url} />
+            <p>First Name: {props.first_name}</p>
+            <p>Last Name: {props.last_name}</p>
+            <p>Gender: {props.gender}</p>
+          </div>
+        </div>
+    </Link>
   );
 };
 
