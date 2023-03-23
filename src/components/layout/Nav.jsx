cc7bcd84831b335dec1795d38a93f0ec35c36841
jceroma23 <jestceroma88@gmail.com>
2023-03-23 21:47:27 +0800

    Need Fix

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -4,12 +4,10 @@ import { Link } from 'react-router-dom';
 import { useSignOut } from 'react-auth-kit'
 import ModalPost from './ModalPost';
 import SearchBox from '../searchBox/SearchBox';
-function Nav({resultState}) {
   
-
-
-const Nav = ({ props }) => {
+const Nav = ({ props, resultState }) => {
   const signOut = useSignOut()
+
   return (
     <header className="pt-1 fixed-top navbarC border">
       <div className="container">
@@ -101,32 +99,10 @@ const Nav = ({ props }) => {
             </li>
             {/* END PROFILE */}
             </ul>
-
-          {/* <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-2 link-secondary">Overview</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Inventory</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Customers</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>
-          </ul> */}
-
-
-
-          {/* <div class="dropdown text-end">
-            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-            </a>
-            <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-              <li><a class="dropdown-item" href="#">New project...</a></li>
-              <li><a class="dropdown-item" href="#">Settings</a></li>
-              <li><a class="dropdown-item" href="#">Profile</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Sign out</a></li>
-            </ul>
-          </div> */}
         </div>
       </div>
     </header>
   )
 }
 
-export default Nav;
+export default Nav
