31815f3a90806a538c486a5258f79d5d123525e7
Jonathan C Dela Peña <jonathancastillondelapena28@gmail.com>
2023-03-24 07:56:53 +0800

    Minor routing

--- src/components/common/CardProfile.jsx
+++ src/components/common/CardProfile.jsx
@@ -1,36 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 export const CardProfile = ({ props }) => {
-
-    const [text, setText] = useState('Add friend');
-        let userType = 'user';
-        useEffect(() => {
-            if (userType === 'user') {
-            setText('Edit Profile');
-            }
-        }, [userType]);
+  const [text, setText] = useState("Add friend");
+  let userType = "user";
+  useEffect(() => {
+    if (userType === "user") {
+      setText("Edit Profile");
+    }
+  }, [userType]);
 
   return (
-            <div className="left-col">
-                <div className="img-container">
-                    <img src={props.image_url} className="pic" />
-                    <span></span>
-                </div>
-                <h2>{props.first_name} {props.last_name}</h2>
-                <button className="btn btn-primary ms-auto ">{text}</button>
-                <ul className="about">
-                    <li><span>300</span>Friends</li>
-                    <li><span>10,074</span>Post</li>
-                </ul>
-                <div className="content">
-                    My Other Social Media
-                    <ul>
-                        <li><i className="bi bi-twitter"></i></li>
-                        <li><i className="bi bi-facebook"></i></li>
-                        <li><i className="bi bi-instagram"></i></li>
-                    </ul>
-                </div>
-            </div>
-
-  )
-}
+    <div className="left-col">
+      <div className="img-container">
+        <img src={props.image_url} className="pic" />
+        <span></span>
+      </div>
+      <h2>
+        {props.first_name} {props.last_name}
+      </h2>
+      <button className="btn btn-primary ms-auto ">
+        <a href="/changeEmail" style={{color: "inherit"}}>{text}</a>
+      </button>
+      <ul className="about">
+        <li>
+          <span>300</span>Friends
+        </li>
+        <li>
+          <span>10,074</span>Post
+        </li>
+      </ul>
+      <div className="content">
+        My Other Social Media
+        <ul>
+          <li>
+            <i className="bi bi-twitter"></i>
+          </li>
+          <li>
+            <i className="bi bi-facebook"></i>
+          </li>
+          <li>
+            <i className="bi bi-instagram"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
